0b248609e45844db6b7cb233f14a94a5af24a524
john <xelasel.rotmen2010@gmail.com>
2020-02-07 17:51:03 +0300

    Initialization new project. Create POM and refactoring application.properties by application.yml

---
+++ src/main/java/com/example/demosecurejdbcrest/DemoSecureJdbcRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosecurejdbcrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecureJdbcRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSecureJdbcRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demosecurejdbcrest/DemoSecureJdbcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosecurejdbcrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecureJdbcRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
